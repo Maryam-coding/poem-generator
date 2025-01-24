b1da29ca30e023c7846b185ca88e69b2cb0a0623
Maryam-coding <maryamnoor97@gmail.com>
2025-01-24 11:28:14 -0800

    Added an animation on the CSS

--- src/index.js
+++ src/index.js
@@ -12,19 +12,20 @@ function generatePoem(event) {
     event.preventDefault();
 
     let instructionInput = document.querySelector("#user-instruction");
-
     let apiKey = "c474d77002fc0f7bt699a6356o32d5ad";
     let context = "You are a poem expert and you love to write short poems, your mission is to generate a four line poem. Make sure to follow user instrctions";
     let prompt = `User instructions are: Generate a poem about ${instructionInput.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    console.log("generating poem");
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">Generating a poem about ${instructionInput.value}</div>`;
+
+    console.log("Generating poem");
     console.log(`Context: ${context}`);
     console.log(`Prompt: ${prompt}`);
 
     axios.get(apiUrl).then(displayPoem);
-
-
 }    
 
 let poemFormElement = document.querySelector("#poem-generator-form");
