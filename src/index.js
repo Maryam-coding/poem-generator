5c6f567ed095ef1017b950c5e14c7929cf4e663b
Maryam-coding <maryamnoor97@gmail.com>
2025-01-24 11:45:44 -0800

    Adjusted the margins on the HTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response){
-    console.log("Poem Generated");
     new Typewriter('#poem', {
         strings: response.data.answer,
         autoStart: true,
@@ -19,11 +18,7 @@ function generatePoem(event) {
 
     let poemElement = document.querySelector("#poem");
     poemElement.classList.remove("hidden");
-    poemElement.innerHTML = `<div class="generating">Generating a poem about ${instructionInput.value}</div>`;
-
-    console.log("Generating poem");
-    console.log(`Context: ${context}`);
-    console.log(`Prompt: ${prompt}`);
+    poemElement.innerHTML = `<div class="generating"> Generating a poem about ${instructionInput.value}</div>`;
 
     axios.get(apiUrl).then(displayPoem);
 }    
